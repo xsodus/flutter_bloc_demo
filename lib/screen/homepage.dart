d8764bdf76c3ce9deb36e9dd9929a87e79315eb8
AkkaponPalo <118162092+AkkaponPalo@users.noreply.github.com>
2022-12-02 09:21:23 +0700

    Refactor code

--- lib/screen/homepage.dart
+++ lib/screen/homepage.dart
@@ -1,12 +1,8 @@
 import 'package:flutter_bloc_demo/widget/counter_view.dart';
-import 'package:flutter_bloc_demo/cubit/global_cubit.dart';
-import 'package:flutter_bloc_demo/cubit/global_state.dart';
 import 'package:flutter_bloc_demo/cubit/text_cubit.dart';
 import 'package:flutter/material.dart';
 
-import 'package:bloc/bloc.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_bloc_demo/observer/counter_observer.dart';
 import 'package:flutter_bloc_demo/cubit/counter_cubit.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
